c73a438b17cf062b7c8410e554f500115e2fec82
Yutyrannus <geek418@gmail.com>
2016-05-08 15:14:44 +0800

    增加“我的版面”中的今日贴数显示

--- www/js/controllers/myBoard.js
+++ www/js/controllers/myBoard.js
@@ -32,4 +32,27 @@ angular.module('cc98')
       }
       return uri;
     };
+
+    //根据板块今日帖数的多少来确定label的颜色
+    $scope.labelColor = function (postCount) {
+      if (postCount != undefined) {
+        if (postCount == 0)
+          return "label-default";
+        else if (postCount < 10)
+          return "label-info";
+        else if (postCount < 50)
+          return "label-primary";
+        else if (postCount < 100)
+          return "label-success";
+        else if (postCount < 200)
+          return "label-energized";
+        else if (postCount < 500)
+          return "label-royal";
+        else if (postCount < 1000)
+          return "label-pink";
+        else
+          return "label-danger";
+      }
+    }
+
   });
